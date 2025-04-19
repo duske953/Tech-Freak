5679aef2cbda6bb91b4ba00a0f0bc0a0f63dafb6
Eloho Kennedy <kennyduske@gmail.com>
2025-04-19 20:11:51 +0100

    remove unused element

--- app/(Auth)/account/page.tsx
+++ app/(Auth)/account/page.tsx
@@ -1,7 +1,6 @@
 import InitAccount from '../components/InitAccount';
 import { redirect } from 'next/navigation';
 import getLoggedUser from '@/app/utils/getLoggedUser';
-import ErrorPage from '@/app/components/ErrorPage';
 
 export default async function Page() {
   const [response] = await getLoggedUser();
