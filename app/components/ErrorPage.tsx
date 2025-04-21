a7a38e81b93e360e12c698bd0a469d3d695c2a3f
Eloho Kennedy <kennyduske@gmail.com>
2025-04-21 22:20:39 +0100

    update ui of error page

--- app/components/ErrorPage.tsx
+++ app/components/ErrorPage.tsx
@@ -3,28 +3,32 @@ import Link from 'next/link';
 import { buttonVariants } from './ui/button';
 
 export default function ErrorPage({
-  msg = 'Oops! Looks like this page packed its bags and left. Maybe try another route?',
+  msg = "Something went wrong. That's all we know",
 }: {
   msg?: string;
 }) {
   return (
-    <div className="flex justify-center flex-col gap-4 items-center py-4 px-2">
+    <div className="flex justify-center flex-col gap-4 items-center py-12 px-2">
       <Image
         src="/err.svg"
         alt="Something went wrong with this request"
         height={500}
         width={500}
       />
-      <p className="relative text-2xl font-bold text-center">{msg}</p>
-      <Link
-        href="/"
-        className={`${buttonVariants({
-          size: 'lg',
-          variant: 'secondary',
-        })} mt-4`}
-      >
-        Home
-      </Link>
+      <div className="flex flex-col gap-3">
+        <p className="relative text-3xl font-bold text-center text-gray-600">
+          {msg}
+        </p>
+        <Link
+          href="/"
+          className={`${buttonVariants({
+            size: 'lg',
+            variant: 'outline',
+          })} mt-4`}
+        >
+          Home
+        </Link>
+      </div>
     </div>
   );
 }
