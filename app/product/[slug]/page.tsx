55c9d3cab25f29b7c483a7ccd497d185c92effe9
Eloho Kennedy <kennyduske@gmail.com>
2025-04-24 21:55:26 +0100

    show Errorpage on err

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -47,7 +47,7 @@ export default async function Page({ searchParams }: pageProps) {
   if (!id) return <ErrorPage />;
   const [response, err] = await tryCatchGet(`products/fetchProduct/${id}`, 0);
 
-  if (err) return <div>{err}</div>;
+  if (err) return <ErrorPage />;
 
   const productData = response.data.product as productTypes;
   return (
